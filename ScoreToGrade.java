98cadb1690b3ce9d31668d8dd5c915dceaa61381
johrielrsrd <johrielrsrd@gmail.com>
2023-11-10 21:50:21 +0800

    updated the conditions

--- ScoreToGrade.java
+++ ScoreToGrade.java
@@ -4,11 +4,11 @@ public class ScoreToGrade {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Please enter score: ");
+        System.out.print("Please enter a score (0-100): ");
         int score = scan.nextInt();
         scan.close();
 
-        if (score >= 90)
+        if (score >= 90 && score <= 100)
             System.out.println("You got an A grade!");
         else if (score <= 89 && score >= 80)
             System.out.println("You got a B grade!");
@@ -16,7 +16,9 @@ public class ScoreToGrade {
             System.out.println("You got a C grade!");
         else if (score <= 69 && score >= 60)
             System.out.println("You got a D grade!");
-        else
+        else if (score <= 59 && score >= 0)
             System.out.println("You got an F grade!");
+        else
+            System.out.println("Input is out of range.");
     }
 }
